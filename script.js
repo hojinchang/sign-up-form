d1b2d5dc1fe5fe1c5bf54b6ef971471c9e1a947e
hojinchang <hojinchang1998@gmail.com>
2023-09-17 18:50:09 -0700

    Add password logic
    Add code which checks if the password and confirmation passwords are the same.

--- script.js
+++ script.js
@@ -11,6 +11,19 @@ function displayErrorMessage(errorField, message) {
     errorField.textContent = message;
 }
 
+function checkPasswords(password, errorFieldPass, passwordConfirm, errorFieldPassConfirm) {
+    if (passwordConfirm !== undefined) {
+        if (password === passwordConfirm) {
+            displayErrorMessage(errorFieldPass, "");
+            displayErrorMessage(errorFieldPassConfirm, "");
+        } else {
+            displayErrorMessage(errorFieldPass, "Password does not match");
+            displayErrorMessage(errorFieldPassConfirm, "Password does not match");
+        }
+    }
+}
+
+
 inputEmail.addEventListener("blur", (e) => {
     let input = e.target.value;
     let errorField = errorFields[2];
@@ -27,6 +40,40 @@ inputPhoneNumber.addEventListener("blur", (e) => {
     let errorField = errorFields[3];
 
     const regex = /^\d{10}$|^\d{3}-\d{3}-\d{4}$/;
-    console.log(regex.test(input))
     regex.test(input) || input === "" ? displayErrorMessage(errorField, "") : displayErrorMessage(errorField, "*Please enter a valid phone number");
+})
+
+let password;
+inputPassword.addEventListener("blur", (e) => {
+    let input = e.target.value;
+    let errorField = errorFields[4];
+
+    if (input.length < 8 & input.length != 0) {
+        displayErrorMessage(errorField, "*Password must be minimum 8 characters long");
+    } else {
+        displayErrorMessage(errorField, "");
+        password = input;
+
+        checkPasswords(password, errorFields[4], passwordConfirm, errorFields[5]);
+    }
+})
+
+let passwordConfirm;
+inputPasswordConfirm.addEventListener("blur", (e) => {
+    let input = e.target.value;
+    let errorFieldPass = errorFields[4];
+    let errorFieldPassConfirm = errorFields[5];
+
+    if (input === "") {
+        displayErrorMessage(errorFieldPass, "");
+        displayErrorMessage(errorFieldPassConfirm, "");
+    } else if (password !== input) {
+        displayErrorMessage(errorFieldPass, "Password does not match");
+        displayErrorMessage(errorFieldPassConfirm, "Password does not match");
+    } else {
+        displayErrorMessage(errorFieldPass, "");
+        displayErrorMessage(errorFieldPassConfirm, "");
+    }
+
+    passwordConfirm = input;
 })
