b0c53a90f38d1809f35401c7cdbe37514afc395e
hojinchang <hojinchang1998@gmail.com>
2023-09-17 22:12:45 -0700

    Refractor code

--- script.js
+++ script.js
@@ -14,18 +14,39 @@ function displayErrorMessage(errorField, message) {
 function checkEmail(email, errorField) {
     if (email.includes("@") && (email.includes(".com") || email.includes(".org") || email.includes(".net")) || email === "") {
         displayErrorMessage(errorField, "");
+        return true;
     } else {
         displayErrorMessage(errorField, "*Please enter a valid Email");
+        return false;
     }
 }
 
 function checkPhoneNumber(number, errorField) {
     const regex = /^\d{10}$|^\d{3}-\d{3}-\d{4}$/;
-    regex.test(number) || number === "" ? displayErrorMessage(errorField, "") : 
-                                        displayErrorMessage(errorField, "*Please enter a valid phone number");
+
+    if (regex.test(number) || number === "") {
+        displayErrorMessage(errorField, "");
+        return true;
+    } else {
+        displayErrorMessage(errorField, "*Please enter a valid phone number");
+        return false;
+    }
 }
 
-function checkPasswords(password, errorFieldPass, passwordConfirm, errorFieldPassConfirm) {
+function checkPassword(password, errorField) {
+    if (password.length < 8 && password.length !== 0) {
+        displayErrorMessage(errorField, "*Password must be minimum 8 characters long");
+        return false;
+    } else if (password.length === 0) {
+        displayErrorMessage(errorField, "");
+        return false;
+    } else {
+        displayErrorMessage(errorField, "");
+        return true;
+    }
+}
+
+function comparePasswords(password, errorFieldPass, passwordConfirm, errorFieldPassConfirm) {
     if (password === passwordConfirm) {
         displayErrorMessage(errorFieldPass, "");
         displayErrorMessage(errorFieldPassConfirm, "");
@@ -37,20 +58,38 @@ function checkPasswords(password, errorFieldPass, passwordConfirm, errorFieldPas
     }
 }
 
+function updateForm(form, inputField, validInput) {
+    if (validInput) {
+        form[inputField] = true;
+    } else {
+        form[inputField] = false;
+    }
+}
 
+
+let completeForm = {
+    firstName: false,
+    lastName: false,
+    email: false,
+    phoneNumber: false,
+    password: false,
+    passwordConfirm: false,
+};
 
 inputEmail.addEventListener("blur", (e) => {
     let input = e.target.value;
     let errorField = errorFields[2];
     
-    checkEmail(input, errorField);
+    let validEmail = checkEmail(input, errorField);
+    updateForm(completeForm, "email", validEmail);
 })
 
 inputPhoneNumber.addEventListener("blur", (e) => {
     let input = e.target.value;
     let errorField = errorFields[3];
 
-    checkPhoneNumber(input, errorField)
+    let validPhoneNumber = checkPhoneNumber(input, errorField);
+    updateForm(completeForm, "phoneNumber", validPhoneNumber);
 })
 
 let password;
@@ -59,15 +98,19 @@ inputPassword.addEventListener("blur", (e) => {
     let input = e.target.value;
     let errorField = errorFields[4];
 
-    if (input.length < 8 & input.length != 0) {
-        displayErrorMessage(errorField, "*Password must be minimum 8 characters long");
-    } else {
-        displayErrorMessage(errorField, "");
+    let validPassword = checkPassword(input, errorField);
+    if (validPassword) {
         password = input;
     }
+    updateForm(completeForm, "password", validPassword);
 
     if (passwordConfirm !== undefined) {
-        passwordsMatch = checkPasswords(password, errorFields[4], passwordConfirm, errorFields[5]);
+        passwordsMatch = comparePasswords(password, errorFields[4], passwordConfirm, errorFields[5]);
+        if (passwordsMatch) {
+            completeForm["passwordConfirm"] = true;
+        } else {
+            completeForm["passwordConfirm"] = false;
+        }
     }
 })
 
@@ -77,11 +120,11 @@ inputPasswordConfirm.addEventListener("blur", (e) => {
     let errorFieldPass = errorFields[4];
     let errorFieldPassConfirm = errorFields[5];
 
-    passwordsMatch = checkPasswords(password, errorFieldPass, input, errorFieldPassConfirm);
-    passwordConfirm = input;
-})
-
-
-submitButton.addEventListener("click", (e) => {
-    
+    passwordsMatch = comparePasswords(password, errorFieldPass, input, errorFieldPassConfirm);
+    if (passwordsMatch) {
+        passwordConfirm = input;
+        completeForm["passwordConfirm"] = true;
+    } else {
+        completeForm["passwordConfirm"] = false;
+    }
 })
